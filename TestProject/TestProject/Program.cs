e7734bf4615c1b9c16c404b93c7997cd33287634
iliangeorgiev <ilian.iliev81@gmail.com>
2017-02-06 14:57:54 +0200

    greeting Git Bash!

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -11,6 +11,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git Hub");
+            Console.WriteLine("Hello Git Bash");
         }
     }
 }
