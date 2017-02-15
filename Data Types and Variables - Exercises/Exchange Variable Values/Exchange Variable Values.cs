dbc79445169ae77a340a2db2e09f67f9093ef4a3
iliangeorgiev <ilian.iliev81@gmail.com>
2017-02-15 18:26:45 +0200

    Exchange Variable Values

---
+++ Data Types and Variables - Exercises/Exchange Variable Values/Exchange Variable Values.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Exchange_Variable_Values
+{
+   public class Program
+    {
+       public static void Main()
+        {
+        int a = 5;
+        int b = 10;
+        
+        Console.WriteLine(b);
+        Console.WriteLine(a);
+
+
+        }
+    }
+}
