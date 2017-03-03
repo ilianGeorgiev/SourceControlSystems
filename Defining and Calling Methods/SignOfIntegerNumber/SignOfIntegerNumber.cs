e0c3f1dcd6aab641b77801c7b6ed1cf34dc07012
iliangeorgiev <ilian.iliev81@gmail.com>
2017-03-03 09:41:24 +0200

    SignOfIntegerNumber.cs

---
+++ Defining and Calling Methods/SignOfIntegerNumber/SignOfIntegerNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignOfIntegerNumber
+{
+    class SignOfIntegerNumber
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            PrintSign(n);
+        }
+
+        static void PrintSign(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+
+            else
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+        }
+    }
+}
